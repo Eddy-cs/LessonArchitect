112ab1c9646b0eb28932bf8d89b052d23712ae1e
Eddy-cs <102255380+Eddy-cs@users.noreply.github.com>
2022-12-02 20:00:23 -0500

    Modified about description

--- pages/about.js
+++ pages/about.js
@@ -7,14 +7,12 @@ export default function About() {
   return (
     <Fragment>
       <div className={styles.about}>
-        <Typography variant="h4">
-          LessonArchitect is a tool to help teachers spend less time planing and
-          more inspiring students.
-        </Typography>
-        <Typography>
-          This app uses the GPT-3 AI model to demonstrate a simple but
-          interesting use case of this powerful technology. If you are more
-          interested check the openAI website for more information.
+        <Typography variant="h4">Helping Teachers Teach</Typography>
+        <Typography variant="h5">
+          I understand the challenges teachers face when it comes to lesson
+          planning. My mission is to provide quick and easy solutions to save
+          teachers time and energy, allowing them to focus on making meaningful
+          connections with their students and inspiring them to learn.
         </Typography>
       </div>
       <div

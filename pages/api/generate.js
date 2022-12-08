c28871794befcf9201437c1906e9744907697b68
Eddy-cs <102255380+Eddy-cs@users.noreply.github.com>
2022-12-07 19:39:05 -0500

    Refactored variable names and functions for readability

--- pages/api/generate.js
+++ pages/api/generate.js
@@ -6,11 +6,53 @@ const configuration = new Configuration({
 });
 const openai = new OpenAIApi(configuration);
 
-let allowRequest = true;
+export default async function openAiCreate(req, res) {
+  await checkRequestMax(req.body.uid);
+  checkRequestLength(
+    req.body.generatedLesson.grade,
+    req.body.generatedLesson.subject,
+    req.body.generatedLesson.lesson
+  );
+  if (allowRequest === true) {
+    const completion = await openai.createCompletion({
+      model: "text-davinci-002",
+      prompt: generatePrompt(
+        req.body.generatedLesson.grade,
+        req.body.generatedLesson.subject,
+        req.body.generatedLesson.lesson
+      ),
+      temperature: 0.85,
+      top_p: 1,
+      max_tokens: 600,
+    });
+    const response = completion.data.choices[0].text;
+    const filterL = await contenFilter(response);
 
-function checkRequestLength(reqGenre, reqTopic, reqTheme) {
-  if (reqGenre.length > 50 || reqTopic.length > 25 || reqTheme.length > 25) {
-    allowRequest = false;
+    if (filterL == "0" || filterL == "1") {
+      const lessonData = {
+        uid: req.body.uid,
+        displayName: req.body.displayName,
+        email: req.body.email,
+        photoURL: req.body.photoURL,
+        generatedLesson: {
+          lesson: req.body.generatedLesson.lesson,
+          subject: req.body.generatedLesson.subject,
+          grade: req.body.generatedLesson.grade,
+          generatedLesson: response,
+        },
+      };
+      addData(lessonData);
+      res.status(200).json({ result: response });
+    } else {
+      res
+        .status(200)
+        .json({ result: "Please try again by modifying the input." });
+    }
+  } else {
+    res.status(200).json({
+      result:
+        "Sorry, the maximum number of requests for today has been reached. Please sign in or try again tomorrow.",
+    });
   }
 }
 
@@ -23,69 +65,26 @@ async function checkRequestMax(userid) {
   for (let d = 0; d < data.length; d++) {
     let time = data[d].timestamp;
     let date = new Date(time.seconds * 1000).toLocaleDateString("en-US");
-    if (userid === "null") {
-      if (date == today) {
-        allRequests++;
-      }
-    } else {
-      if (date == today) {
-        allRequests++;
-      }
-      if (date == today && data[d].uid === userid) {
+    if (date == today) {
+      allRequests++;
+      if (userid !== "null" && data[d].uid === userid) {
         userRequests++;
       }
     }
   }
 
-  if (userid === "null") {
-    if (allRequests >= 10) {
-      allowRequest = false;
-    }
-  } else {
-    if (allRequests >= 30) {
-      allowRequest = false;
-    } else {
-      if (userRequests >= 5) {
-        allowRequest = false;
-      }
-    }
+  if (userid === "null" && allRequests >= 10) {
+    allowRequest = false;
+  } else if (allRequests >= 30 || userRequests >= 5) {
+    allowRequest = false;
   }
 }
 
-export default async function openAiCreate(req, res) {
-  await checkRequestMax(req.body.uid);
-  checkRequestLength(req.body.genre, req.body.topic, req.body.theme);
-  if (allowRequest === true) {
-    const completion = await openai.createCompletion({
-      model: "text-davinci-002",
-      prompt: generatePrompt(req.body.genre, req.body.topic, req.body.theme),
-      temperature: 0.85,
-      top_p: 1,
-      max_tokens: 600,
-    });
-    const response = completion.data.choices[0].text;
-    const filterL = await contenFilter(response);
+let allowRequest = true;
 
-    if (filterL == "0" || filterL == "1") {
-      const storyData = {
-        uid: req.body.uid,
-        genre: req.body.genre,
-        user: req.body.user,
-        title: `${req.body.theme}`,
-        story: response,
-      };
-      addData(storyData);
-      res.status(200).json({ result: response });
-    } else {
-      res
-        .status(200)
-        .json({ result: "Please try again by modifying the input." });
-    }
-  } else {
-    res.status(200).json({
-      result:
-        "Sorry, the maximum number of requests for today has been reached. Please sign in or try again tomorrow.",
-    });
+function checkRequestLength(reqGrade, reqSubject, reqTheme) {
+  if (reqGrade.length > 50 || reqSubject.length > 25 || reqTheme.length > 25) {
+    allowRequest = false;
   }
 }
 
@@ -103,6 +102,6 @@ async function contenFilter(resp) {
   return filterResponse.data["choices"][0]["text"];
 }
 
-function generatePrompt(genre, topic, theme) {
-  return `A lesson plan for a ${genre} class, subject ${topic}, lesson${theme}\n`;
+function generatePrompt(grade, subject, theme) {
+  return `A lesson plan for a ${grade} class, subject ${subject}, lesson${theme}\n`;
 }

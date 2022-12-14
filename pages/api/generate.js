182f6d796b1d958315483f504f7d126b86b28ba2
Eddy-cs <eddysr.cs@gmail.com>
2022-12-13 23:47:55 -0500

    Added comments for readability

--- pages/api/generate.js
+++ pages/api/generate.js
@@ -6,6 +6,60 @@ const configuration = new Configuration({
 });
 const openai = new OpenAIApi(configuration);
 
+let allowRequest = true;
+
+// Checks and limits the maximum number of requests per day
+async function checkRequestMax(userid) {
+  const data = await getData();
+  const today = new Date().toLocaleDateString();
+  let allRequests = 0;
+  let userRequests = 0;
+
+  for (let d = 0; d < data.length; d++) {
+    const time = data[d].timestamp;
+    const date = new Date(time.seconds * 1000).toLocaleDateString("en-US");
+    if (date == today) {
+      allRequests++;
+      if (userid !== "null" && data[d].uid === userid) {
+        userRequests++;
+      }
+    }
+  }
+
+  if (userid === "null" && allRequests >= 10) {
+    allowRequest = false;
+  } else if (allRequests >= 30 || userRequests >= 5) {
+    allowRequest = false;
+  }
+}
+
+// Checks that client input data doesn't exeedes allowed maximum
+function checkRequestLength(reqGrade, reqSubject, reqTheme) {
+  if (reqGrade.length > 50 || reqSubject.length > 25 || reqTheme.length > 25) {
+    allowRequest = false;
+  }
+}
+
+// Request to content-filter-alpha to filter inappropriate content
+async function contenFilter(resp) {
+  const filterResponse = await openai
+    .createCompletion({
+      model: "content-filter-alpha",
+      prompt: `<|endoftext|>${resp}\n--\nLabel:`,
+      max_tokens: 1,
+      temperature: 0,
+      top_p: 0,
+      logprobs: 10,
+    })
+    .catch((error) => {});
+  return filterResponse.data["choices"][0]["text"];
+}
+
+// Concatenates string for openAI request
+function generatePrompt(grade, subject, theme) {
+  return `A lesson plan for a ${grade} class, subject ${subject}, lesson${theme}\n`;
+}
+
 export default async function openAiCreate(req, res) {
   await checkRequestMax(req.body.uid);
   checkRequestLength(
@@ -13,7 +67,6 @@ export default async function openAiCreate(req, res) {
     req.body.generatedLesson.subject,
     req.body.generatedLesson.lesson
   );
-  console.log(req.body.generatedLesson.randomness);
   if (allowRequest === true) {
     const completion = await openai.createCompletion({
       model: "text-davinci-002",
@@ -29,6 +82,7 @@ export default async function openAiCreate(req, res) {
     const response = completion.data.choices[0].text;
     const filterL = await contenFilter(response);
 
+    // Checks if response contains inappropriate content based on contentFilter()
     if (filterL == "0" || filterL == "1") {
       const lessonData = {
         uid: req.body.uid,
@@ -55,54 +109,4 @@ export default async function openAiCreate(req, res) {
         "Sorry, the maximum number of requests for today has been reached. Please sign in or try again tomorrow.",
     });
   }
-}
-
-async function checkRequestMax(userid) {
-  const data = await getData();
-  const today = new Date().toLocaleDateString();
-  let allRequests = 0;
-  let userRequests = 0;
-
-  for (let d = 0; d < data.length; d++) {
-    let time = data[d].timestamp;
-    let date = new Date(time.seconds * 1000).toLocaleDateString("en-US");
-    if (date == today) {
-      allRequests++;
-      if (userid !== "null" && data[d].uid === userid) {
-        userRequests++;
-      }
-    }
-  }
-
-  if (userid === "null" && allRequests >= 10) {
-    allowRequest = false;
-  } else if (allRequests >= 30 || userRequests >= 5) {
-    allowRequest = false;
-  }
-}
-
-let allowRequest = true;
-
-function checkRequestLength(reqGrade, reqSubject, reqTheme) {
-  if (reqGrade.length > 50 || reqSubject.length > 25 || reqTheme.length > 25) {
-    allowRequest = false;
-  }
-}
-
-async function contenFilter(resp) {
-  const filterResponse = await openai
-    .createCompletion({
-      model: "content-filter-alpha",
-      prompt: `<|endoftext|>${resp}\n--\nLabel:`,
-      max_tokens: 1,
-      temperature: 0,
-      top_p: 0,
-      logprobs: 10,
-    })
-    .catch((error) => {});
-  return filterResponse.data["choices"][0]["text"];
-}
-
-function generatePrompt(grade, subject, theme) {
-  return `A lesson plan for a ${grade} class, subject ${subject}, lesson${theme}\n`;
 }

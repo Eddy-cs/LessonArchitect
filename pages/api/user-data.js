b3c413ab507f0c31bc506d1b93de4880e657f878
Eddy-cs <eddysr.cs@gmail.com>
2022-12-15 21:22:12 -0500

    Added endpoint to display user lessons

---
+++ pages/api/user-data.js
@@ -0,0 +1,12 @@
+import { getData } from "./firebase-config";
+
+export default async function getClientData(req, res) {
+  if (req.method === "POST") {
+    const allDocuments = await getData();
+    for (let i = 0; i < allDocuments.length; i++) {
+      if (allDocuments[i].uid === req.body.uid) {
+        res.status(200).json(allDocuments[i].generatedLessons);
+      }
+    }
+  }
+}
--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,37 @@
 import styles from "../styles/index.module.css";
-import { Fragment } from "react";
-import { Card, CardContent, Typography } from "@mui/material";
+import { Fragment, useEffect, useState } from "react";
+import LessonList from "../components/LessonList";
+import { auth } from "../components/Login";
+import { useAuthState } from "react-firebase-hooks/auth";
 
 function UserLessons() {
+  const [user] = useAuthState(auth);
+  const [lessons, setLessons] = useState();
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    async function getLessons() {
+      const response = await fetch("/api/user-data", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(user),
+      });
+      const data = await response.json();
+      setLessons(data.map((e) => ({ generatedLessons: [e] })));
+      setIsLoading(true);
+    }
+    getLessons();
+  }, [user]);
+
   return (
     <Fragment>
-      Create a Lesson
+      {isLoading === true ? (
+        <LessonList lessons={lessons} pageTitle={"My Lessons"} />
+      ) : (
+        <div>Loading...</div>
+      )}
       <div className={styles.lessons__create}>
         <div className={styles.lessons__center}>
           <div className={styles.lessons__plus}></div>
